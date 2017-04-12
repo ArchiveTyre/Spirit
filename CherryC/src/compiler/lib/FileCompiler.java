be71e288412b4441efaf4eaa7e0d1cc6bd4b77ab
davidwolters <david@wolters.se>
2017-04-13 00:09:40 +0200

    Created FileCompiler class

--- CherryC/src/compiler/lib/FileCompiler.java
+++ CherryC/src/compiler/lib/FileCompiler.java
@@ -5,6 +5,7 @@ import compiler.Parser;
 import compiler.ast.ASTClass;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.InputStream;
 import java.io.PushbackInputStream;
 import java.nio.charset.StandardCharsets;
@@ -17,22 +18,63 @@ public class FileCompiler
 
 	public static void compileFile(String fileName)
 	{
-		String content = FileHandler.getFileContents(fileName);
+		if (shouldCompile(fileName))
+		{
+			String content = FileHandler.getFileContents(fileName);
 
-		String[] splitFileName = fileName.split("\\.");
-		String className = splitFileName[0];
-
-
-		InputStream inputStream = new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));
-
+			String[] splitFileName = fileName.split("\\.");
+			String className = splitFileName[0];
 
 
-		PushbackInputStream pushbackInputStream = new PushbackInputStream(inputStream);
+			InputStream inputStream = new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));
 
-		Lexer lexer = new Lexer(pushbackInputStream, fileName);
 
-		Parser parser = new Parser(lexer);
+			PushbackInputStream pushbackInputStream = new PushbackInputStream(inputStream);
 
-		parser.parseFile(new ASTClass(className, null));
+			Lexer lexer = new Lexer(pushbackInputStream, fileName);
+
+			Parser parser = new Parser(lexer);
+
+			parser.parseFile(new ASTClass(className, null));
+		}
+	}
+
+	public static void createClassFiles(String fileName)
+	{
+		// TODO: Implement this using FileHandler.createFile();
+	}
+
+	/**
+	 * Checks if we need to compile a file by comparing the output file to the source files last modified date.
+	 * @param fileName The name of the file
+	 * @return If we should compile the file or not.
+	 */
+	public static boolean shouldCompile(String fileName)
+	{
+		// Create a reference to the two files, source and cpp output. //
+		File sourceFile = new File(fileName);
+		File cppFile = new File("out/" + fileName + ".cpp");
+
+		// If the source file does not exist then we have a problem. //
+		if (!sourceFile.exists())
+		{
+			return false;
+		}
+
+
+		// If the cpp file does not exist then we need to compile. //
+		if (!cppFile.exists())
+		{
+			return true;
+		}
+
+		// If we changed the source file since the last compile, then we need to recompile. //
+		if (sourceFile.lastModified() > cppFile.lastModified())
+		{
+			return true;
+		}
+
+		// Otherwise we do not need to compile this. //
+		return false;
 	}
 }
--- CherryC/src/compiler/lib/FileHandler.java
+++ CherryC/src/compiler/lib/FileHandler.java
@@ -10,6 +10,11 @@ import java.nio.file.Paths;
 public class FileHandler
 {
 
+	/**
+	 * Fetches the content a file.
+	 * @param fileName The name of the file to read.
+	 * @return The content of the file.
+	 */
 	public static String getFileContents(String fileName)
 	{
 		try
@@ -21,5 +26,24 @@ public class FileHandler
 			e.printStackTrace();
 		}
 		return null;
+	}
+
+	/**
+	 * Creates an empty file.
+	 * @param fileName The name of the file.
+	 * @return If the creation of the file was successful.
+	 */
+	public static boolean createFile(String fileName)
+	{
+		try
+		{
+			Files.write(Paths.get(fileName), new byte[]{});
+			return true;
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
